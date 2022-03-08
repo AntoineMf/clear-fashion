f42e03fb6398b2c4ac66996d298cb1937499b312
AntoineMf <itsezax@gmail.com>
2022-03-08 19:04:57 +0100

    scrapping

--- server/sandbox.js
+++ server/sandbox.js
@@ -9,12 +9,20 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 
     console.log(products);
     console.log('done');
+    products.forEach(products => {
+      console.log(products.name);
+    })
     process.exit(0);
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
+
+
+const products = dedicatedbrand.scrape('https://www.dedicatedbrand.com/en/men/news');
+
+
 
 const [,, eshop] = process.argv;
 
