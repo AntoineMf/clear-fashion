02929535dffaba6e49ede7388e0b7b91adc5a2c2
AntoineMf <itsezax@gmail.com>
2022-03-12 11:22:40 +0100

    api

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const db = require('./db');
 
 const PORT = 8092;
 
@@ -13,9 +14,14 @@ app.use(cors());
 app.use(helmet());
 
 app.options('*', cors());
+app.get('/product', async(request, response) => {
+  
+  response.send({'ack':true})
+});
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+app.get('/product/:id', async(request, response) => {
+  const loomOnly = await db.find({'_id':request.params.id});
+  response.send(loomOnly);
 });
 
 app.listen(PORT);
